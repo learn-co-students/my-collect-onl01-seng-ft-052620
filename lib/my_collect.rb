d5ed5d8ab9d22b4ee78a193583c5514c46df44e7
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 23:55:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    result = []
+  
+    while i < array.length
+      result << yield(array[i])
+      i += 1
+    end
+  else
+    "No block given"
+  end
+  result
+end
