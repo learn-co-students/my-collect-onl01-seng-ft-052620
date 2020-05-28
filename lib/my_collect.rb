0c362e1045279da8d74742c857c93bf18d73c925
jessegan <jesse13579@gmail.com>
2020-05-28 09:02:39 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(arr)
+    #Create counter variable for index in arr
+    i = 0 
+    #Create new empty collection to store returns of the block
+    rtn = []
 
-
+    while i < arr.length
+        rtn << yield(arr[i])
+        i+=1
+    end
+    rtn
+end 
