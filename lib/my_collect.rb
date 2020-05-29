eaec6e60c5f853697b0dc2e3e56f8daa0000e4aa
Maloochow <luyao.mly@gmail.com>
2020-05-29 13:55:33 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,27 +1,50 @@
-# require 'pry'
+require 'pry'
+
+# def my_collect(array)
+# binding.pry
+# if array.length == 0
+#     return 0
+
+# elsif
+
+#     array[0].include?(" ")
+
+#     array.map do |element|
+#     element.split(" ").first
+# end
+
+# else
+
+#     array.map do |content|
+#     content.upcase
+#     end
+
+# end
+
+# end
+
 
 def my_collect(array)
-
-if array.length == 0
-    return 0
-
-elsif
-
-    array[0].include?(" ")
-
-    array.map do |element|
-    element.split(" ").first
-end
-
-else
-
-    array.map do |content|
-    content.upcase
+    new_array = []
+    # if array.size == 0
+    #     return 0
+    # else
+    counter = 0
+    while counter < array.length
+        new_array << yield(array[counter])
+        counter += 1
     end
-
+    
+    new_array
 end
 
-end
+# my_collect() do |element|
+#     if element.include?(" ")
+#         element.split(" ").first
+#     else
+#         content.upcase
+#     end
+# end
 
 
 
