b502ab61704354901ce4451fac6831d4f32744f7
Eyerus K <3yerus@gmail.com>
2020-05-30 04:02:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require "pry"
+def my_collect(lang) 
+   new_col = []
+   i = 0
+  while lang.length > i
+  puts new_col << yield(lang[i])
+  i += 1
+end
+new_col
+  end 
+  
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |uplang|
+  uplang.upcase
+end
 
