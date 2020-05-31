fcde8e79b910693f64b1d74f8565f31accbf7ede
LetsJessCode <letsjesscode@gmail.com>
2020-05-31 17:23:02 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(list)
+    modified_list = []
+    index = 0
+
+while index < list.length
+    modified_list << yield(list[index])
+    index += 1
+    end
+
+modified_list
+end 
 
 
