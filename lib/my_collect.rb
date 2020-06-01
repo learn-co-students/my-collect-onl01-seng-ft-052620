31760a749203423f05b972500635a5b02362ef35
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-01 22:07:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(array)
+  
+  modified_array = []
+  index = 0 
+  
+  while index < array.length 
+    modified_array << yield(array[index])
+    index += 1 
+  end 
+  
+  modified_array 
+end
 
 
+
+
+
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+
+# returns "Tim", "Tom", "Jim"
