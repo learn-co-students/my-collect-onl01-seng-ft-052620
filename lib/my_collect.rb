b18bcfbb56e8ac18cf5925c4d4427f1f1d61ee61
Kayla Kingston <kkingston09@gmail.com>
2020-06-02 00:46:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
 
 
+def my_collect(array)
+  new_array = []
+  index = 0 
+
+  while index < array.length 
+    new_array << yield(array[index])
+    index += 1 
+  end
+  new_array
+end 
+
+# ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# # end
+
+# ["Tim", "Tom", "Jim"]
